d82c7453d834e03fc6ec849335a362a709d5e747
Candymini <candyzhouziqing@hotmail.com>
2017-04-28 23:08:02 +0800

    Test

---
+++ app/src/main/java/com/liyuaninc/liyuan/activity/Register.java
@@ -0,0 +1,15 @@
+package com.liyuaninc.liyuan.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.liyuaninc.liyuan.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
