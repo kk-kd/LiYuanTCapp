294389a39e3f59d51f154154af8a363e0158ca08
Rarcher <2553263392@qq.com>
2017-04-30 23:28:00 +0800

    login

--- app/src/main/java/com/liyuaninc/liyuan/Register.java
+++ app/src/main/java/com/liyuaninc/liyuan/Register.java
@@ -1,19 +1,25 @@
 package com.liyuaninc.liyuan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 import db.HttpUtils;
 import db.NetTool;
 import db.finalnet;
 
+import static com.liyuaninc.liyuan.R.id.cancel_action;
 import static com.liyuaninc.liyuan.R.id.imageView;
 import static db.finalnet.sendGet;
 
@@ -24,6 +30,13 @@ public class Register extends AppCompatActivity {
     public EditText register_password;
     public Button   submit;
     private static Calendar c = Calendar.getInstance();
+    private boolean intentl;
+
+    public static final int UPDATE_TEXT = 1;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,23 +54,41 @@ public class Register extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                new Thread(new Runnable(){
+                new Thread() {
+                    public void run() {
+                        //这儿是耗时操作，完成之后更新UI；
+                        finalnet finalnet = new finalnet();
 
-                    @Override public void run() {
-                    finalnet finalnet = new finalnet();
+                        String username = register_name.getText().toString();
+                        String useremail = register_email.getText().toString();
+                        String userpassword = register_password.getText().toString();
 
-                    String username = register_name.getText().toString();
-                    String useremail = register_email.getText().toString();
-                    String userpassword = register_password.getText().toString();
+                        final String theparam="uname="+username+"&upwd="+userpassword+"&umail="+useremail;
+                        String result = finalnet.sendPost(API,theparam);
+                        Log.d("HttpUtils",result);
+                        Log.d("theparam",theparam);
+                        switch (result){
+                            case "ok":intentl=true;
+                                break;
+                            default:intentl=false;
+                                break;
+                        }
+                        runOnUiThread(new Runnable(){
+                            @Override
+                            public void run() {
+                                //更新UI
+                                if (intentl==true){
+                                    Intent intent =new Intent(Register.this,user.class);
+                                    startActivity(intent);
+                                    finish();}
+                                else {
+                                    Toast.makeText(Register.this,"注册失败",Toast.LENGTH_SHORT).show();
+                                }
+                            }
 
-                    final String theparam="uname="+username+"&upwd="+userpassword+"&umail="+useremail;
-                    String result = finalnet.sendPost(API,theparam);
-
-                    Log.d("HttpUtils",result);
-                    Log.d("theparam",theparam);
-
-                }
-                }).start();
+                        });
+                    }
+                }.start();
 
             }
         });
