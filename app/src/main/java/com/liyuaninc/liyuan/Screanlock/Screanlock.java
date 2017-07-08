4fea633b983b4aba2c48a428809b57abaceee4a2
Rarcher <2553263392@qq.com>
2017-07-08 22:03:57 +0800

    rarcher fragment switch test finish and add the screenclock activity

---
+++ app/src/main/java/com/liyuaninc/liyuan/Screanlock/Screanlock.java
@@ -0,0 +1,18 @@
+package com.liyuaninc.liyuan.Screanlock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.liyuaninc.liyuan.R;
+
+public class Screanlock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screanlock);
+
+
+
+    }
+}
--- app/src/main/java/com/liyuaninc/liyuan/spinmenu/Fragment8.java
+++ app/src/main/java/com/liyuaninc/liyuan/spinmenu/Fragment8.java
@@ -7,6 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.liyuaninc.liyuan.R;
 
@@ -14,6 +19,8 @@ import com.liyuaninc.liyuan.R;
  *
  */
 public class Fragment8 extends Fragment {
+private Switch switchclock;
+    private TextView text;
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -22,22 +29,47 @@ public class Fragment8 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frament_8, container, false);
+       View view=inflater.inflate(R.layout.frament_8, container, false);
+        return view;
+
+
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+text=(TextView)getView().findViewById(R.id.startlock) ;
+switchclock=(Switch)getView().findViewById(R.id.lockon);
+        switchclock.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+text.setText("on");
+                }
+                else {
+                    text.setText("off");
+                }
+            }
+        });
+
     }
 
     public static Fragment8 newInstance() {
         Fragment8 fragment8 = new Fragment8();
         return fragment8;
     }
+
+
+
 }
 
