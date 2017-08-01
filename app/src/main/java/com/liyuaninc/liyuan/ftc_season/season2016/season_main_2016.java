7e01319fccf1e9ee3af2a83ae85e6e3ae5be62c8
Rarcher <2553263392@qq.com>
2017-08-01 22:20:44 +0800

    new pic,new activity

---
+++ app/src/main/java/com/liyuaninc/liyuan/ftc_season/season2016/season_main_2016.java
@@ -0,0 +1,15 @@
+package com.liyuaninc.liyuan.ftc_season.season2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.liyuaninc.liyuan.R;
+
+public class season_main_2016 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_season_main_2016);
+    }
+}
--- app/src/main/java/com/liyuaninc/liyuan/spinmenu/Fragment2.java
+++ app/src/main/java/com/liyuaninc/liyuan/spinmenu/Fragment2.java
@@ -1,25 +1,38 @@
 package com.liyuaninc.liyuan.spinmenu;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.liyuaninc.liyuan.R;
+import com.liyuaninc.liyuan.help.opmodehelp;
 
 public class Fragment2 extends Fragment {
+    private ImageButton go2016;
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        go2016.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),com.liyuaninc.liyuan.ftc_season.season2016.season_main_2016.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frament_2, container, false);
+        View view = inflater.inflate(R.layout.frament_2, container, false);
+        go2016 = (ImageButton)view.findViewById(R.id.go2016);
+        return view;
     }
 
     @Override
