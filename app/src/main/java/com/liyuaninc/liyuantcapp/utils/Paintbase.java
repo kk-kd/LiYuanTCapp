7bb7efd7a24da7a26582919a7e49c99d7e084124
Rarchers <2553263392@qq.com>
2018-07-03 21:09:41 +0800

    放弃治疗2.JPG

--- app/src/main/java/com/liyuaninc/liyuantcapp/utils/Paintbase.java
+++ app/src/main/java/com/liyuaninc/liyuantcapp/utils/Paintbase.java
@@ -26,6 +26,13 @@ public class Paintbase extends View {
         rpaint.setTextSize(30);
         Typeface typeFace = Typeface.defaultFromStyle(Typeface.BOLD);
         rpaint.setTypeface(typeFace);
-        canvas.drawText("hello world", 100, 600, rpaint);
+        canvas.drawText("亲爱的，如果你看到这句话" ,100, 600, rpaint);
+        canvas.drawLine(100,800,400,600,rpaint);
+        canvas.drawText("那么表示Rarcher已经凉了" ,100, 650, rpaint);
+        canvas.drawText("而且我知道有一根很丑的线" ,100, 700, rpaint);
+        canvas.drawText("然而并不想修这个bug" ,100, 750, rpaint);
+        canvas.drawText("只想放弃治疗，垃圾paint函数" ,100, 800, rpaint);
+        canvas.drawText("这一堆都是用paint函数写的只想说一句mmp" ,100, 850, rpaint);
+        canvas.drawText("再见这个世界T_T" ,100, 950, rpaint);
     }
 }
